025257d2fb0658a3bb89ecf062dd3dcc8c05c445
Rionaryyy <urukio444@gmail.com>
2025-09-26 21:36:08 +0900

    chore: force update types.ts to fix build

--- app/types/types.ts
+++ app/types/types.ts
@@ -1,11 +1,13 @@
-// app/types/types.ts
-export interface DiagnosisAnswers {
+// Updated types - 2025-09-26
+export type DiagnosisAnswers = {
   includePoints: string;
   qualityPriority: string;
   carrierType: string;
+  supportPreference: string;
+  contractLockPreference: string;
   ecosystemUse: string;
   dataUsage: string;
   callFrequency: string;
   familyDiscount: string;
   bundleDiscount: string;
-}
+};
