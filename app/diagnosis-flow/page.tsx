41c97b5f3c64aefbacc938ef732b6d0d447e355b
Rionaryyy <urukio444@gmail.com>
2025-09-30 21:58:51 +0900

    Fix: add defaultValues to Phase1Props

--- app/diagnosis-flow/page.tsx
+++ app/diagnosis-flow/page.tsx
@@ -3,7 +3,7 @@ import DiagnosisFlow from "../components/DiagnosisFlow";
 
 export default function DiagnosisPage() {
   return (
-    <main className="min-h-screen bg-red-200 py-10 px-4">
+    <main className="min-h-screen bg-gray-50 py-10 px-4">
       <DiagnosisFlow />
     </main>
   );
